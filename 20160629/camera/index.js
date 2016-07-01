3c83cf0c3e737fa3a778e4ef796913ec210ce645
林俊宏 <linjh1012@gmail.com>
2016-07-01 09:10:55 +0800

    照相机

--- 20160629/camera/index.js
+++ 20160629/camera/index.js
@@ -7,8 +7,15 @@
     function Main() {
 
         this._video = document.getElementById("video");
+        this._btnSavePhoto = document.getElementById("btnsavephoto");
+        this._canvas = document.createElement("canvas");
+        this._canvas.width = 1000;
+        this._canvas.height = 1000;
+        this._context2d = this._canvas.getContext("2d");
 
         this.showCamera();
+        this.addListeners();
+        this.render();
     }
 
     Main.prototype.showCamera = function () {
@@ -25,6 +32,25 @@
             console.error(error);
             alert("您拒绝了使用本机设备");
         });
+    };
+
+    Main.prototype.addListeners = function () {
+        this._btnSavePhoto.onclick = function () {
+            var url = this._canvas.toDataURL();
+
+            var a = document.createElement("a");
+            a.href = url;
+            a.target = "_blank";
+            a.innerHTML = "下载图片";
+            document.body.appendChild(a);
+        }.bind(this);
+    };
+
+    Main.prototype.render = function () {
+
+        this._context2d.drawImage(this._video, 0, 0);
+
+        requestAnimationFrame(this.render.bind(this));
     };
 
     new Main();
